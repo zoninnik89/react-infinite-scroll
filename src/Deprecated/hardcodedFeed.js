928ad5d18238e9e96d54cb1bf8579b137f2259a8
Nick Zonin <zoninnik@gmail.com>
2024-06-02 14:03:07 +0200

    v8

---
+++ src/Deprecated/hardcodedFeed.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from 'react';
+import Post from './post';
+import postsMockData from './mockData.js';
+import './styles.css'; 
+
+import useModal from "./useModal";
+import CreatePostPopup from './CreatePostPopup';
+
+const itemHeight = 492; // Adjustable global variable
+const windowHeight = 650; // Adjustable global variable
+const overscan = 4; // Number of extra items to render before the visible range
+let coursor = 0
+
+const url = 'https://10.59.62.240:3001/infscrolldata?coursor=0&limit=10';
+
+export default function Feed() {
+
+  const [data, setData] = useState(null);
+  const [numberOfItems, setNumberOfItems] = useState(10);
+  debugger
+  useEffect(() => {
+    debugger
+    const getPosts = async () => {
+      try {
+        const response = await fetch(url)
+        if (response.ok) {
+          const jsonResponse = await response.json()
+          setData(jsonResponse)
+          console.log(data)
+        } else {
+          console.error(response.statusText)
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    getPosts();
+
+  }, []);
+
+  const [scrollTop, setScrollTop] = useState(0);
+  const startIndex = Math.max(0, Math.floor(scrollTop / itemHeight) - overscan);
+  let renderedNodesCount = Math.floor(windowHeight / itemHeight) + 2 * overscan;
+  renderedNodesCount = Math.min(numberOfItems - startIndex, renderedNodesCount);
+  
+  const posts = data === null ? [] : data.slice(startIndex, startIndex+renderedNodesCount);
+
+
+  const [isShowingModal, toggleModal] = useModal(null);
+
+  if (data === null) {
+    return "Loading..."
+  } else {
+    return (
+        <>
+          <CreatePostPopup
+            show={isShowingModal} 
+            onCloseButtonClick={toggleModal} 
+            onCreatePost={setData}
+            numberOfItems={setNumberOfItems}
+          /> 
+          <button 
+            className='button button-smaller'
+            onClick={toggleModal}>
+              Create post
+          </button>
+          <div className='Feed'
+            style={{ height: `${windowHeight}px` }}
+            onScroll={(e) => {
+              setScrollTop(e.currentTarget.scrollTop);
+            }}
+          >
+            <div
+              style={{
+                height: `${numberOfItems * itemHeight}px`,
+              }}
+            >
+              <div
+                style={{
+                  transform: `translateY(${startIndex * itemHeight}px)`,
+                }}
+              >
+                {posts.map(item=><Post key={item.id} postData={item} />)}
+              </div>
+            </div>
+          </div>
+        </>
+      );
+    }
+}
--- src/components/feed.js
+++ src/components/feed.js
@@ -6,85 +6,80 @@ import './styles.css';
 import useModal from "./useModal";
 import CreatePostPopup from './CreatePostPopup';
 
-const itemHeight = 492; // Adjustable global variable
-const windowHeight = 650; // Adjustable global variable
-const overscan = 4; // Number of extra items to render before the visible range
+import * as ReactDOM from 'react-dom'
+import { Virtuoso } from 'react-virtuoso'
+
 let coursor = 0
 
 const url = 'https://10.59.62.240:3001/infscrolldata?coursor=0&limit=10';
 
 export default function Feed() {
 
-  const [data, setData] = useState(null);
-  const [numberOfItems, setNumberOfItems] = useState(10);
-  debugger
-  useEffect(() => {
-    debugger
-    const getPosts = async () => {
-      try {
-        const response = await fetch(url)
-        if (response.ok) {
-          const jsonResponse = await response.json()
-          setData(jsonResponse)
-          console.log(data)
-        } else {
-          console.error(response.statusText)
-        }
-      } catch (error) {
-        console.error(error)
-      }
-    }
-    getPosts();
-
-  }, []);
-
-  const [scrollTop, setScrollTop] = useState(0);
-  const startIndex = Math.max(0, Math.floor(scrollTop / itemHeight) - overscan);
-  let renderedNodesCount = Math.floor(windowHeight / itemHeight) + 2 * overscan;
-  renderedNodesCount = Math.min(numberOfItems - startIndex, renderedNodesCount);
-  console.log(`start index is ${startIndex}, nodes count is ${renderedNodesCount}`);
-  const posts = data === null ? [] : data.slice(startIndex, startIndex+renderedNodesCount);
-  console.log(`Posts are ${posts}`)
+  const [data, setData] = useState([]);
+  const [numberOfItems, setNumberOfItems] = useState(data.length !== 0 ? data.length : 0);
 
   const [isShowingModal, toggleModal] = useModal(null);
+    
+  const getPosts = async () => {
+    try {
+      const response = await fetch(url)
+      if (response.ok) {
+        const jsonResponse = await response.json()
+        
+        setData((data) => [...data, ...jsonResponse])
 
-  if (data === null) {
-    return "Loading..."
-  } else {
-    return (
-        <>
-          <CreatePostPopup
-            show={isShowingModal} 
-            onCloseButtonClick={toggleModal} 
-            onCreatePost={setData}
-            numberOfItems={setNumberOfItems}
-          /> 
-          <button 
-            className='button button-smaller'
-            onClick={toggleModal}>
-              Create post
-          </button>
-          <div className='Feed'
-            style={{ height: `${windowHeight}px` }}
-            onScroll={(e) => {
-              setScrollTop(e.currentTarget.scrollTop);
-            }}
-          >
-            <div
-              style={{
-                height: `${numberOfItems * itemHeight}px`,
-              }}
-            >
-              <div
-                style={{
-                  transform: `translateY(${startIndex * itemHeight}px)`,
-                }}
-              >
-                {posts.map(item=><Post key={item.id} postData={item} />)}
-              </div>
-            </div>
-          </div>
-        </>
-      );
+      } else {
+        console.error(response.statusText)
+      }
+    } catch (error) {
+      console.error(error)
     }
+  }
+
+  useEffect(() => {
+    const timeout = getPosts()
+    return () => clearTimeout(timeout)
+  }, [])
+
+  const Footer = () => {
+    return (
+      <div
+        style={{
+          padding: '2rem',
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        Loading...
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <CreatePostPopup
+        show={isShowingModal} 
+        onCloseButtonClick={toggleModal} 
+        onCreatePost={setData}
+        numberOfItems={setNumberOfItems}
+      /> 
+      <button 
+        className='button button-smaller'
+        onClick={toggleModal}>
+          Create post
+      </button>
+      <div className='Feed'>
+        <Virtuoso
+          style={{ height: 600 }}
+          data={data}
+          endReached={getPosts}
+          increaseViewportBy={10}
+          itemContent={(index) => {
+            return <div> <Post key={data[index].id} postData={data[index]} /></div>
+          }}
+          components={{ Footer }}
+        />   
+      </div>
+    </>
+  );
 }
